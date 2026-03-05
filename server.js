8d0d202ee84c3a6defe8142d34d3cfa0f1bc2e9b
Crissqu <crissqu@github.com>
2026-03-05 20:13:27 +0800

    update cookie

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // 默认cookie（用户需要提供自己的）
-const DEFAULT_COOKIE = process.env.ZHIHU_COOKIE || 'z_c0=2|1:0|10:1772070981|4:z_c0|92:Mi4xM0NNWEFBQUFBQUNLOU5PbUNKLWRHaVlBQUFCZ0FsVk5OZVdNYWdBODZKdnJscDFQTmtKU3FwaTNRMkVkU2ZMTF9B|6ff339dd03b1bb1f551514c3a896cd921a3dcabaab09fe4b81f7ed7f2dc79032';
+const DEFAULT_COOKIE = process.env.ZHIHU_COOKIE || 'z_c0=2|1:0|10:1770517280|4:z_c0|92:Mi4xM0NNWEFBQUFBQUFOb1ZQQjExOHBHeVlBQUFCZ0FsVk5lWXQwYWdDQW5hYzN5dFU3c0IxS1J4YjdyQTFqV2Y4Qnln|24436b4481d038b1f15e7acf33e1a5c557ac863f8e4fef53c275fe96b0a8b939';
 
 // 通用请求头
 const getHeaders = (cookie) => ({
